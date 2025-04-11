1463b9e99d4d481b56a23ccf81ccca50774c2e6a
Siphelele-Nzama <anelenzama07@gmail.com>
2025-04-11 09:02:05 +0200

    Privacy Page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,5 +17,8 @@ public class HomeController : Controller
     {
         return View();
     }
-
+    public IActionResult Privacy()
+    {
+        return View(); 
+    }
 }
---
+++ Controllers/PatientsController.cs
@@ -0,0 +1,20 @@
+﻿using MediTrackPro.Data;
+using MediTrackPro.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+public class PatientsController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public PatientsController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    // GET: Patients
+    public async Task<IActionResult> Index()
+    {
+        return View(await _context.Patients.ToListAsync());
+    }
+}
